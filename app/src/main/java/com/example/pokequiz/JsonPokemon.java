869efcb830482f6ac38e0bac82354aa92529f914
Felipe Baumgartel <felipe.baumgartel@catolicasc.org.br>
2019-04-16 21:05:20 -0300

    commit

---
+++ app/src/main/java/com/example/pokequiz/JsonPokemon.java
@@ -0,0 +1,20 @@
+package com.example.pokequiz;
+
+public class JsonPokemon {
+    private String name;
+    private String img;
+
+    public JsonPokemon(String name, String img) {
+        this.name = name;
+        this.img = img;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+}
